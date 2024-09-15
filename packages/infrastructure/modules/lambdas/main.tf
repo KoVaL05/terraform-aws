97e8df7e3c4875512ca59a70ca8a941ba670fda5
Filip <91011923+MrSheep05@users.noreply.github.com>
2024-09-15 22:42:16 +0200

    Aws (#3)
    * basic yml
    
    * pre structure
    
    * roles and iam
    
    * region
    
    * another fix

--- packages/infrastructure/modules/lambdas/main.tf
+++ packages/infrastructure/modules/lambdas/main.tf
@@ -8,7 +8,7 @@ resource "aws_iam_role" "lambda_roles" {
 resource "aws_lambda_function" "lambda_functions" {
   for_each = local.lambda_functions_data
 
-  function_name = format("%s-%s", each.key, var.random_name)
+  function_name = format("%s_%s", each.key, var.random_name)
   role          = aws_iam_role.lambda_roles[each.key].arn
   runtime       = "python3.9"
   timeout       = 60
