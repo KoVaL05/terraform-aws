95d6f4187bc36600a134e912bd6434fd18999cd8
Filip Wachowski <fifiw2005@gmail.com>
2024-09-16 19:34:55 +0200

    workflow sketch

---
+++ packages/lambdas/subject_group_executor/index.py
@@ -0,0 +1,28 @@
+import json
+
+def handler(event, context):
+    agent = event['agent']
+    actionGroup = event['actionGroup']
+    function = event['function']
+    parameters = event.get('parameters', [])
+    print("EVENT",event)
+    # Execute your business logic here. For more information, refer to: https://docs.aws.amazon.com/bedrock/latest/userguide/agents-lambda.html
+    responseBody =  {
+        "TEXT": {
+            "body": "The function {} was called successfully!".format(function)
+        }
+    }
+
+    action_response = {
+        'actionGroup': actionGroup,
+        'function': function,
+        'functionResponse': {
+            'responseBody': responseBody
+        }
+
+    }
+
+    dummy_function_response = {'response': action_response, 'messageVersion': event['messageVersion']}
+    print("Response: {}".format(dummy_function_response))
+
+    return dummy_function_response
