c0c112019c84752deaa4681119a71f0171e31d87
Filip <91011923+MrSheep05@users.noreply.github.com>
2024-09-18 19:07:27 +0200

    S# Bucket ACL update (#16)
    * basic yml
    
    * s3 acl

--- packages/infrastructure/modules/s3/main.lambda.tf
+++ packages/infrastructure/modules/s3/main.lambda.tf
@@ -5,7 +5,7 @@ resource "aws_s3_bucket" "lambdas_bucket" {
 resource "aws_s3_bucket_public_access_block" "lambdas_bucket_public_access" {
   bucket = aws_s3_bucket.lambdas_bucket.id
 
-  block_public_acls       = true
+  block_public_acls       = false
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
@@ -26,5 +26,5 @@ resource "aws_s3_bucket_acl" "lambdas_bucket_acl" {
   ]
 
   bucket = aws_s3_bucket.lambdas_bucket.id
-  acl    = "private"
+  acl    = "public-read"
 }
