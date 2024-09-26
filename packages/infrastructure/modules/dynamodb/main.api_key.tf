47a4e068a03f123b5ffef62693013a5795bbe0d6
Filip <91011923+MrSheep05@users.noreply.github.com>
2024-09-26 22:24:58 +0200

    indexes (#26)

--- packages/infrastructure/modules/dynamodb/main.api_key.tf
+++ packages/infrastructure/modules/dynamodb/main.api_key.tf
@@ -3,7 +3,8 @@ resource "aws_dynamodb_table" "api_key_table" {
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "ID"
 
-  stream_enabled = true
+  stream_enabled   = true
+  stream_view_type = "NEW_AND_OLD_IMAGES"
 
   attribute {
     name = "ID"
@@ -43,5 +44,13 @@ resource "aws_dynamodb_table" "api_key_table" {
     range_key          = "KeyType"
     projection_type    = "INCLUDE"
     non_key_attributes = ["ApiKey", "Timestamp"]
+  }
+
+  global_secondary_index {
+    name               = "APIKeyIndex"
+    hash_key           = "ApiKey"
+    range_key          = "UID"
+    projection_type    = "INCLUDE"
+    non_key_attributes = ["KeyType", "Timestamp"]
   }
 }
