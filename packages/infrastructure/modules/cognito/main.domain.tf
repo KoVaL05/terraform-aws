a94200af719a588cbd5b0ad1134e4fd381ebcf01
Filip Wachowski <fifiw2005@gmail.com>
2024-09-27 21:12:08 +0200

    domain fix

--- packages/infrastructure/modules/cognito/main.domain.tf
+++ packages/infrastructure/modules/cognito/main.domain.tf
@@ -1,4 +1,4 @@
 resource "aws_cognito_user_pool_domain" "default_domain" {
-  domain = format("aws-test-%s",var.random_name)
+  domain = format("test-%s",var.random_name)
   user_pool_id = aws_cognito_user_pool.default_user_pool.id
 }
