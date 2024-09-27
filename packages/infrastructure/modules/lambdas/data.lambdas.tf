d434777a166825f2a7c582e3818a469877b18e43
Filip Wachowski <fifiw2005@gmail.com>
2024-09-27 21:24:43 +0200

    without outflows

--- packages/infrastructure/modules/lambdas/data.lambdas.tf
+++ packages/infrastructure/modules/lambdas/data.lambdas.tf
@@ -2,5 +2,5 @@ data "aws_s3_object" "object" {
   for_each = local.lambda_functions_data
 
   bucket = var.lambdas_bucket_name
-  key = format("%s/%s.zip", each.key, each.key)
+  key    = format("%s/%s.zip", each.key, each.key)
 }
