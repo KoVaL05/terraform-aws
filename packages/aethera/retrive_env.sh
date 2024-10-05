cb98b64c7749123006615b5f9bc09625aa7fe57a
Filip Wachowski <fifiw2005@gmail.com>
2024-10-05 18:39:41 +0200

    retrive env

--- packages/aethera/retrive_env.sh
+++ packages/aethera/retrive_env.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 SECRETS=$(aws secretsmanager get-secret-value --secret-id aethera_flutter_app --query SecretString --output text)
 
-echo "VERSION = $(echo $SECRETS | jq -r .version)" > .env
+echo "USER_POOL_ID = $(echo $SECRETS | jq -r .userPoolId)" > .env
+echo "CLIENT_POOL_ID = $(echo $SECRETS | jq -r .clientPoolId)" >> .env
