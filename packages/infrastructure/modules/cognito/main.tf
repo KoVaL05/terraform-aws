f11ec67fc146e8d03deb9c5a2a8608caac034f55
Filip <91011923+MrSheep05@users.noreply.github.com>
2024-10-09 17:15:49 +0200

    Attributes mapping (#41)

--- packages/infrastructure/modules/cognito/main.tf
+++ packages/infrastructure/modules/cognito/main.tf
@@ -72,9 +72,7 @@ resource "aws_cognito_user_pool_client" "default_client" {
   user_pool_id    = aws_cognito_user_pool.default_user_pool.id
   generate_secret = false
 
-  # allowed_oauth_flows_user_pool_client = true
-  # allowed_oauth_flows                  = ["implicit"]
-  # allowed_oauth_scopes                 = ["profile", "email", "openid"]
+  allowed_oauth_scopes                 = ["profile", "email", "openid"]
 
   explicit_auth_flows = ["ALLOW_REFRESH_TOKEN_AUTH", "ALLOW_CUSTOM_AUTH", "ALLOW_USER_SRP_AUTH", "ALLOW_USER_PASSWORD_AUTH"]
 
