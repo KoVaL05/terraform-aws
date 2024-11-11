4bf4c4314491b595badcf0484186ec5375861b10
Filip Wachowski <fifiw2005@gmail.com>
2024-11-11 16:26:19 +0100

    fix

--- packages/infrastructure/modules/policies/main.sns.tf
+++ packages/infrastructure/modules/policies/main.sns.tf
@@ -1,4 +1,4 @@
 resource "aws_sns_topic_policy" "sns_topic_policy" {
-  arn    = aws_sns_topic.secrets_manager_topic.arn
+  arn    = var.secret_manager_sns_arn
   policy = data.aws_iam_policy_document.secret_manager_sns_policy.json
 }
