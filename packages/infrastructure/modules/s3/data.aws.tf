db50fa11a7f2701bc8b8cb8613f5db4f1ae2afaa
Filip <91011923+MrSheep05@users.noreply.github.com>
2024-11-14 20:33:35 +0100

    fix (#64)

--- packages/infrastructure/modules/s3/data.aws.tf
+++ packages/infrastructure/modules/s3/data.aws.tf
@@ -1,5 +1,1 @@
 data "aws_caller_identity" "current" {}
-
-data "aws_region" "current" {}
-
-data "aws_partition" "current" {}
--- packages/infrastructure/modules/s3/data.cloud_trail.tf
+++ packages/infrastructure/modules/s3/data.cloud_trail.tf
@@ -10,11 +10,6 @@ data "aws_iam_policy_document" "cloud_trail_policy" {
 
     actions   = ["s3:GetBucketAcl"]
     resources = [aws_s3_bucket.cloud_trail_bucket.arn]
-    condition {
-      test     = "StringEquals"
-      variable = "aws:SourceArn"
-      values   = ["arn:${data.aws_partition.current.partition}:cloudtrail:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:trail/*"]
-    }
   }
 
   statement {
@@ -27,17 +22,12 @@ data "aws_iam_policy_document" "cloud_trail_policy" {
     }
 
     actions   = ["s3:PutObject"]
-    resources = ["${aws_s3_bucket.cloud_trail_bucket.arn}/*/AWSLogs/${data.aws_caller_identity.current.account_id}/*"]
+    resources = ["${aws_s3_bucket.cloud_trail_bucket.arn}/AWSLogs/${data.aws_caller_identity.current.account_id}/*"]
 
     condition {
       test     = "StringEquals"
       variable = "s3:x-amz-acl"
       values   = ["bucket-owner-full-control"]
-    }
-    condition {
-      test     = "StringEquals"
-      variable = "aws:SourceArn"
-      values   = ["arn:${data.aws_partition.current.partition}:cloudtrail:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:trail/*"]
     }
   }
 }
