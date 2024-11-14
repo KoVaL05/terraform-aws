9d0025711bfb525ac2cac30af5168403ea4f457d
Filip Wachowski <fifiw2005@gmail.com>
2024-11-14 20:14:59 +0100

    s3 permissions

--- packages/infrastructure/modules/s3/main.cloud_trail.tf
+++ packages/infrastructure/modules/s3/main.cloud_trail.tf
@@ -1,3 +1,9 @@
 resource "aws_s3_bucket" "cloud_trail_bucket" {
   bucket = format("cloud-trail-bucket-%s", var.random_name)
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_policy" "example" {
+  bucket = aws_s3_bucket.cloud_trail_bucket
+  policy = data.aws_iam_policy_document.cloud_trail_policy.json
 }
