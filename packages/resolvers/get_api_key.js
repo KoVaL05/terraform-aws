16865a12dd4e5fcd11b63238510b4890d92667db
Filip Wachowski <fifiw2005@gmail.com>
2024-12-05 20:47:59 +0100

    resolver

---
+++ packages/resolvers/get_api_key.js
@@ -0,0 +1,18 @@
+import { util } from "@aws-appsync/utils";
+import { get } from "@aws-appsync/utils/dynamodb";
+
+export function request(ctx) {
+  const { id } = ctx.args;
+  const key = { id };
+  return get({
+    key,
+  });
+}
+
+export function response(ctx) {
+  const { error, result } = ctx;
+  if (error) {
+    return util.appendError(error.message, error.type, result);
+  }
+  return result;
+}
