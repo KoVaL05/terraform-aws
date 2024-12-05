e41b5c014eccd768b4b53a4ccb58e3bb3b4f6975
Filip <91011923+MrSheep05@users.noreply.github.com>
2024-12-05 20:51:39 +0100

    Appsync (#69)
    * userSignUp lambda
    
    * etag
    
    * resolver

--- packages/infrastructure/modules/s3/cloud_trail.tf
+++ packages/infrastructure/modules/s3/cloud_trail.tf
@@ -1,18 +1,18 @@
-resource "aws_cloudtrail" "sm_cloud_trail" {
-  name                          = "sm_event_manager"
-  s3_bucket_name                = aws_s3_bucket.cloud_trail_bucket.id
-  enable_logging                = true
-  include_global_service_events = false
+# resource "aws_cloudtrail" "sm_cloud_trail" {
+#   name                          = "sm_event_manager"
+#   s3_bucket_name                = aws_s3_bucket.cloud_trail_bucket.id
+#   enable_logging                = true
+#   include_global_service_events = false
 
-  advanced_event_selector {
-    name = "SecretsManagerSelector"
-    field_selector {
-      field         = "eventCategory"
-      equals        = ["Management"]
-    }
-    field_selector {
-      field  = "eventSource"
-      equals = ["secretsmanager.amazonaws.com"]
-    }
-  }
-}
+#   advanced_event_selector {
+#     name = "SecretsManagerSelector"
+#     field_selector {
+#       field         = "eventCategory"
+#       equals        = ["Management"]
+#     }
+#     field_selector {
+#       field  = "eventSource"
+#       equals = ["secretsmanager.amazonaws.com"]
+#     }
+#   }
+# }
--- packages/infrastructure/modules/s3/data.cloud_trail.tf
+++ packages/infrastructure/modules/s3/data.cloud_trail.tf
@@ -1,33 +1,33 @@
-data "aws_iam_policy_document" "cloud_trail_policy" {
-  statement {
-    sid    = "AWSCloudTrailAclCheck"
-    effect = "Allow"
+# data "aws_iam_policy_document" "cloud_trail_policy" {
+#   statement {
+#     sid    = "AWSCloudTrailAclCheck"
+#     effect = "Allow"
 
-    principals {
-      type        = "Service"
-      identifiers = ["cloudtrail.amazonaws.com"]
-    }
+#     principals {
+#       type        = "Service"
+#       identifiers = ["cloudtrail.amazonaws.com"]
+#     }
 
-    actions   = ["s3:GetBucketAcl"]
-    resources = [aws_s3_bucket.cloud_trail_bucket.arn]
-  }
+#     actions   = ["s3:GetBucketAcl"]
+#     resources = [aws_s3_bucket.cloud_trail_bucket.arn]
+#   }
 
-  statement {
-    sid    = "AWSCloudTrailWrite"
-    effect = "Allow"
+#   statement {
+#     sid    = "AWSCloudTrailWrite"
+#     effect = "Allow"
 
-    principals {
-      type        = "Service"
-      identifiers = ["cloudtrail.amazonaws.com"]
-    }
+#     principals {
+#       type        = "Service"
+#       identifiers = ["cloudtrail.amazonaws.com"]
+#     }
 
-    actions   = ["s3:PutObject"]
-    resources = ["${aws_s3_bucket.cloud_trail_bucket.arn}/AWSLogs/${data.aws_caller_identity.current.account_id}/*"]
+#     actions   = ["s3:PutObject"]
+#     resources = ["${aws_s3_bucket.cloud_trail_bucket.arn}/AWSLogs/${data.aws_caller_identity.current.account_id}/*"]
 
-    condition {
-      test     = "StringEquals"
-      variable = "s3:x-amz-acl"
-      values   = ["bucket-owner-full-control"]
-    }
-  }
-}
+#     condition {
+#       test     = "StringEquals"
+#       variable = "s3:x-amz-acl"
+#       values   = ["bucket-owner-full-control"]
+#     }
+#   }
+# }
