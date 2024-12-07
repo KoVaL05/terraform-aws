7603ae555c0e7674fe7fc520b9875e55d4945b4c
Filip Wachowski <fifiw2005@gmail.com>
2024-12-07 17:31:49 +0100

    locals resolver object

---
+++ packages/infrastructure/modules/appsync/main.resolvers.tf
@@ -0,0 +1,20 @@
+resource "aws_appsync_resolver" "unit_kind_resolvers" {
+  for_each = {
+    for key, value in local.resolvers : key => value
+    if value.kind == "UNIT"
+  }
+
+  api_id      = aws_appsync_graphql_api.api_key_public.id
+
+  kind        = "UNIT"
+  type        = each.value.type
+  data_source = each.value.data_source
+  field       = each.key
+  code        = file(format("%s/%s",path.root,each.value.path))
+
+
+  runtime {
+    name            = "APPSYNC_JS"
+    runtime_version = "1.0.0"
+  }
+}
---
+++ packages/resolvers/get_all_api_keys.js
@@ -0,0 +1,21 @@
+import { util } from "@aws-appsync/utils";
+import { scan } from "@aws-appsync/utils/dynamodb";
+
+export function request(ctx) {
+  const { filter, limit, nextToken } = ctx.args;
+
+  return scan({
+    limit,
+    nextToken,
+    filter,
+  });
+}
+
+export function response(ctx) {
+  const { error, result } = ctx;
+  if (error) {
+    return util.appendError(error.message, error.type, result);
+  }
+  const { items = [], nextToken } = result;
+  return { items, nextToken };
+}
